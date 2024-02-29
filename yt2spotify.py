474b30d2466ef95f04817466530fa63042ead0f8
tars919 <138830040+tars919@users.noreply.github.com>
2024-02-29 11:49:31 -0500

    Create yt2spotify.py
    Rough skelton of the class with the function definitons

---
+++ yt2spotify.py
@@ -0,0 +1,30 @@
+#Project to automate spotify from youtube
+
+class CreatePlaylist:
+
+
+    def __init__(self):
+        pass
+
+    #log into youtube 
+    def get_youtube_client(self):
+        pass
+
+    
+    # grab our liked videos
+    def get_liked_vids(self):
+        pass
+
+    #create a new playlist
+    def create_playlist(self):
+        pass
+
+
+    #search for the song
+    def get_spotify_url(self):
+        pass
+
+
+    #add this song into the new spotify playlist
+    def add_song(self):
+        pass
